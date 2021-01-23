cac54bb261da93a4380737b25108bc9693698099
Jake Dam <robert.dam2@gmail.com>
2021-01-23 10:41:38 -0500

    updated choose_word, refactored

--- game.rb
+++ game.rb
@@ -1,28 +1,37 @@
 class Game
   include Display
+  attr_reader :secret_word, :guessed_letters, :incorrect_letters, :display_letters
 
   def initialize(words)
     @words = words
     @secret_word
     @guesses = 10
-  end
-
-  def secret_word
-    @secret_word
+    @guessed_letters, @incorrect_letters, @display_letters = [], [], []
   end
 
   def choose_word
     while word = @words.sample
       if word.length > 4 && word.length < 13
-        @secret_word = word.downcase.chomp
+        @secret_word = word.downcase.chomp.split("")
+        word.length.times { @display_letters << "_" }
         break
       end
     end
   end
 
   def guess_letter
-    #TO DO
+    while letter = get_guess
+      if @guessed_letters.include?(letter)
+        puts "You've already guessed that letter!"
+      else  
+        @guessed_letters << letter
+      end
+    end
+  letter
   end
+
+
+
 end
 
   
