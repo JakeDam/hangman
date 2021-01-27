c3ce94d6797c37adf05c7e84d1b7418829283126
Jake Dam <robert.dam2@gmail.com>
2021-01-26 20:52:58 -0500

    refactored with rubocop

--- display.rb
+++ display.rb
@@ -1,5 +1,7 @@
-module Display 
+# frozen_string_literal: true
 
+# Module handles display of most game info to user
+module Display
   def display_word(letters)
     letters.each { |letter| print "#{letter} " }
     print "\n"
@@ -11,18 +13,16 @@ module Display
   end
 
   def get_guess
-    puts "Enter a letter to guess: "
-    while guess = gets.chomp.downcase
-      if guess !~ /^[a-z]*$/ 
-        puts "Please enter only letters."
+    puts 'Enter a letter to guess: '
+    while (guess = gets.chomp.downcase)
+      if guess !~ /^[a-z]*$/
+        puts 'Please enter only letters.'
       elsif guess.length > 1
-        puts "Please enter only a single letter."
-      else  
+        puts 'Please enter only a single letter.'
+      else
         break
       end
     end
     guess
   end
-
 end
-
