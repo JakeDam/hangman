37df7315c8ee2e3f99bff152f2348f130e3a5757
Jake Dam <robert.dam2@gmail.com>
2021-02-11 18:31:15 -0500

    drafted simple save game method using yaml

--- main.rb
+++ main.rb
@@ -2,12 +2,17 @@
 
 require_relative 'display'
 require_relative 'game'
+require 'yaml'
 
 def new_game
   words = File.readlines('5desk.txt')
   game = Game.new(words)
   game.choose_word
   game.play
+end
+
+def save_game(game)
+  YAML.dump(game)
 end
 
 new_game
