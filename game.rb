e919df1009be420b90317f2f8064e237812fa29f
Jake Dam <robert.dam2@gmail.com>
2021-02-15 21:28:27 -0500

    reorganized files

--- game.rb
+++ game.rb
@@ -5,7 +5,7 @@ class Game
   include Display
   attr_reader :secret_word, :guessed_letters, :incorrect_letters, :display_letters
 
-  WORDS = File.readlines('../5desk.txt')
+  WORDS = File.readlines('5desk.txt')
 
   def initialize
     @guesses = 10
@@ -20,7 +20,7 @@ class Game
 
     yaml = YAML.dump(self)
     puts yaml
-    File.open(File.join(Dir.pwd, "../saved_games/#{filename}.yaml"), 'w+') { |file| file.write(yaml) }
+    File.open(File.join(Dir.pwd, "saved_games/#{filename}.yaml"), 'w+') { |file| file.write(yaml) }
     puts 'Game Saved'
     exit
   end
