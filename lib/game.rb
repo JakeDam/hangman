df730511fb9ce91ebe49ceae72c7ae6041533b38
Jake Dam <robert.dam2@gmail.com>
2021-02-15 21:06:02 -0500

    moved files into lib folder

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,7 @@ class Game
   include Display
   attr_reader :secret_word, :guessed_letters, :incorrect_letters, :display_letters
 
-  WORDS = File.readlines('5desk.txt')
+  WORDS = File.readlines('../5desk.txt')
 
   def initialize
     @guesses = 10
